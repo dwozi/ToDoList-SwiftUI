97cbb0340f29bc530189b096e8a4c02d1e51cc08
Hakan Hardal <hakanhardal@icloud.com>
2024-02-17 18:47:54 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Hakan Hardal on 17.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
